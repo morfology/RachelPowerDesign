3f5dfdd21d83da0573d51a4406e0acf3d1240415
marcpster <15250562+marcpster@users.noreply.github.com>
2024-03-12 16:06:03 +0000

    Add some comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+// Globally used layout.tsx
 import SearchModal from "@/components/SearchModal";
 import config from "@/config/config.json";
 import theme from "@/config/theme.json";
-import TwSizeIndicator from "@/helpers/TwSizeIndicator";
+import TwSizeIndicator from "@/helpers/TwSizeIndicator"; // debug indicator
 import Footer from "@/partials/Footer";
 import Header from "@/partials/Header";
 import Providers from "@/partials/Providers";

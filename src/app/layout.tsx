a8f95a9c77033fe40c175d7fde1b0ce03432ddac
marcpster <15250562+marcpster@users.noreply.github.com>
2024-03-18 16:38:06 +0000

    Remove meta theme bits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,19 +28,6 @@ export default function RootLayout({
 
         {/* favicon */}
         <link rel="shortcut icon" href={config.site.favicon} />
-        {/* theme meta */}
-        <meta name="theme-name" content="nextplate" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: light)"
-          content="#fff"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#000"
-        />
 
         {/* google font css */}
         <link

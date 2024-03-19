71abecbaf4ba272b92d5ad98f7c540d55440a4c5
Marc <15250562+marcpster@users.noreply.github.com>
2024-03-19 11:56:18 +0000

    Tweaks and updates (#8)
    * Remove meta theme bits
    
    * Deep menu removed. Comment for active menu.
    
    * WIP: first attempt at close menu
    
    * Close menu on any click
    
    * Work in Progress using timeout
    
    * Tidy menu close

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -5,7 +5,7 @@ import config from "@/config/config.json";
 import menu from "@/config/menu.json";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { IoSearch } from "react-icons/io5/index.js";
 
 //  child navigation link interface
@@ -23,15 +23,40 @@ export interface INavigationLink {
 }
 
 const Header = () => {
+ 
   // distructuring the main menu from menu object
   const { main }: { main: INavigationLink[] } = menu;
   const { navigation_button, settings } = config;
   // get current path
   const pathname = usePathname();
 
-  // scroll to top on route change
+  // @MP reference the hidden checkbox which shows/hides the menu 
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
+
+    // scroll to top on route change
     window.scroll(0, 0);
+
+    const handleClick = () => {
+ 
+      if (checkboxRef.current && checkboxRef.current.checked) {
+        setTimeout(() => {
+          if (checkboxRef.current && checkboxRef.current) {
+            checkboxRef.current.checked = false;
+          }
+        },
+        100)
+      }
+    };
+
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+
+
   }, [pathname]);
 
   return (
@@ -43,8 +68,8 @@ const Header = () => {
         <div className="order-0">
           <Logo />
         </div>
-        {/* navbar toggler */}
-        <input id="nav-toggle" type="checkbox" className="hidden" />
+        {/* navbar toggler @MP closes menu */}
+        <input id="nav-toggle" type="checkbox" className="hidden" ref={checkboxRef} />
         <label
           htmlFor="nav-toggle"
           className="order-3 cursor-pointer flex items-center lg:hidden text-dark  lg:order-1"
@@ -69,51 +94,19 @@ const Header = () => {
             ></polygon>
           </svg>
         </label>
-        {/* /navbar toggler */}
 
+        {/* navbar menu */}
         <ul
           id="nav-menu"
           className="navbar-nav order-3 hidden w-full pb-6 lg:order-1 lg:flex lg:w-auto lg:space-x-2 lg:pb-0 xl:space-x-8"
         >
           {main.map((menu, i) => (
             <React.Fragment key={`menu-${i}`}>
-              {menu.hasChildren ? (
-                <li className="nav-item nav-dropdown group relative">
-                  <span
-                    className={`nav-link inline-flex items-center ${
-                      menu.children?.map(({ url }) => url).includes(pathname) ||
-                      menu.children
-                        ?.map(({ url }) => `${url}/`)
-                        .includes(pathname)
-                        ? "active"
-                        : ""
-                    }`}
-                  >
-                    {menu.name}
-                    <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </span>
-                  <ul className="nav-dropdown-list hidden group-hover:block lg:invisible lg:absolute lg:block lg:opacity-0 lg:group-hover:visible lg:group-hover:opacity-100">
-                    {menu.children?.map((child, i) => (
-                      <li className="nav-dropdown-item" key={`children-${i}`}>
-                        <Link
-                          href={child.url}
-                          className={`nav-dropdown-link block ${
-                            (pathname === `${child.url}/` ||
-                              pathname === child.url) &&
-                            "active"
-                          }`}
-                        >
-                          {child.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ) : (
+              {(
+              
                 <li className="nav-item">
                   <Link
+
                     href={menu.url}
                     className={`nav-link block ${
                       (pathname === `${menu.url}/` || pathname === menu.url) &&

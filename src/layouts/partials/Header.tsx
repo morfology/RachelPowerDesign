be341728d133cd570d80ef47d3dcedc4897e3475
marcpster <15250562+marcpster@users.noreply.github.com>
2024-04-11 13:10:41 +0100

    Set the Segment js snippet - set "as is" from segment - set using a string not via usual react code

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -6,7 +6,7 @@ import menu from "@/config/menu.json";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState, useRef } from "react";
-
+import SegmentHTML from 'dangerously-set-html-content';
 import { FaSearch as IoSearch } from "react-icons/fa/index.js";
 import { FaPhone } from "react-icons/fa/index.js";
 
@@ -65,6 +65,15 @@ const Header = () => {
     <header
       className={`header z-30 ${settings.sticky_header && "sticky top-0"}`}
     >
+
+      <SegmentHTML html={`<script>` +
+        `!function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="saYBHnKjyZtuwqR9t2ijXvwJoK9g6x9q";;analytics.SNIPPET_VERSION="5.2.0";` +
+        `analytics.load("saYBHnKjyZtuwqR9t2ijXvwJoK9g6x9q");` + 
+        `analytics.page();` +
+      `}}();` + 
+      `</script>`
+      } />
+
       <nav className="navbar container">
         {/* logo */}
         <div className="order-0">

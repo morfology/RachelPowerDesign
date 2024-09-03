d6a366af57651d48ee12e46faee38432fefa7214
marcpster <15250562+marcpster@users.noreply.github.com>
2024-09-03 16:11:12 +0100

    Image updates

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,12 +5,22 @@ import { getListPage } from "@/lib/contentParser";
 import PageHeader from "@/partials/PageHeader";
 import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
+import imageConfig from "@/config/images.json";
 
 // for all regular pages
 const Gallery = () => {
 
   const pageData: RegularPage = getListPage("gallery/_index.md");
   const { title, meta_title, description, image } = pageData.frontmatter;
+
+  let data = null
+  data  = imageConfig    
+    // Filter images that match the detail
+    .filter(x => x.image && x.image.indexOf(`detail`) > 0)
+  ;
+
+
+
 
   return (
     <>
@@ -29,17 +39,10 @@ const Gallery = () => {
 
           {/* Initial Content */}
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <div className="grid gap-4">
-              <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
-              </div>
-            </div>
+            <Card data={data}/>
+
+
+            
             <div className="grid gap-4">
               <div>
                 <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
@@ -70,11 +73,10 @@ const Gallery = () => {
                 <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
               </div>
             </div>
+
+
+
           </div>
-
-
-
-
         </div>
       </section>
     </>
@@ -82,3 +84,28 @@ const Gallery = () => {
 };
 
 export default Gallery;
+
+
+const Card = ({ data }: { data: any }) => {
+  
+  return (
+    <div className="grid gap-4">
+      {data.map(({ image }) => 
+      (<div>
+        <img className="h-auto max-w-full rounded-lg" src={image} alt="" />
+      </div>))}
+
+
+
+    <div>
+      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+    </div>
+    <div>
+      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+    </div>
+    <div>
+      <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+    </div>
+  </div>
+  );
+};

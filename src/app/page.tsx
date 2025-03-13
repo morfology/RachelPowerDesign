2f7961c68714b2d148aadb56eb77117d41f00c92
marcpster <15250562+marcpster@users.noreply.github.com>
2025-03-13 15:22:54 +0000

    Try image change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,29 +40,27 @@ const Home = () => {
 
         {/**** Big page image */}
         <div className="absolute inset-0">
-          {/* <img 
-          src="https://images.unsplash.com/photo-1615875474908-f403116f5287?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxjb2RlfGVufDB8MHx8fDE2OTQwOTg0MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080" alt="Background Image" 
-          className="object-cover object-center w-full h-full" /> */}
-
           <Image
-            className="object-cover object-center w-full h-full"
-            src={'/images/farnham/living-1.jpg'}
-            width={1200}
-            height={500}
-            alt={'Rachel Power Design'}
-            priority={true}
+            className="object-cover object-center"
+            src="/images/farnham/living-1.jpg"
+            alt="Rachel Power Design"
+            priority
+            placeholder="blur"
+            blurDataURL="/images/farnham/living-1-blur.jpg"
+            fill // 👈 This makes it cover the entire parent div!
           />
           <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
+
 
         {/* <ImageSlider data={dataSlider} /> */}
 
 
         {/**** Big Hero Text and strapline */}
         <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
-          <h1 className="mb-4 text-7xl leading-tight text-theme-light"  
+          <h1 className="mb-4 text-7xl leading-tight text-theme-light"
             dangerouslySetInnerHTML={markdownify(banner.title)}
-            />
+          />
           {/* <p className="text-lg text-gray-300 mb-8 max-w-prose"
             dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
             /> */}

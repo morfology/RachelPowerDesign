8a6a846b37e9192801e3bb092d6c5cb1ec673575
marcpster <15250562+marcpster@users.noreply.github.com>
2025-04-30 15:02:49 +0100

    fix for domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import "@/styles/main.css";
 import { Metadata } from "next";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL 
- || process.env.RAILWAY_PUBLIC_DOMAIN
+ || (process.env.RAILWAY_PUBLIC_DOMAIN ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` : '')
  || "http://localhost:3000"
 ;
 

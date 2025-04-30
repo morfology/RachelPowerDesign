fbda483a5b16777ac007d298371a4db0c0bbccfa
marcpster <15250562+marcpster@users.noreply.github.com>
2025-04-30 15:33:24 +0100

    simplify config

--- src/config/site.js
+++ src/config/site.js
@@ -1,41 +1,18 @@
-let showInfo = true;
 
 const siteConfig = {
   // Site Information
-  title: "RPD Interiors",
-  description: "Renovation and interior design services in Surrey",
-
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL 
     || (process.env.RAILWAY_PUBLIC_DOMAIN ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` : '')
-    || "http://localhost:3000",
-
+    || "http://localhost:3000"
+  ,
             
-  // Contact Information
-  email: "info@rpdinteriors.co.uk",
-  phone: "+44 1234 567890",
-  
-  // Social Media
-  social: {
-    instagram: "https://instagram.com/rpdinteriors",
-    facebook: "https://facebook.com/rpdinteriors",
-    pinterest: "https://pinterest.com/rpdinteriors",
-  },
-  
   // SEO
   seo: {
     defaultImage: "/images/og-default.png",
-    twitterHandle: "@rpdinteriors",
-  },
-  
-  // Features
-  features: {
-    contactForm: true,
-    gallery: true,
-    blog: true,
   }
 };
 
-if (showInfo) {
+if (process.env.NODE_ENV !== 'development') {
   const isClient = (typeof window !== 'undefined');
   console.log(`${isClient ? 'Client' : 'Server'} Environment:`, process.env.NODE_ENV);
   console.log(`${isClient ? 'Client' : 'Server'} URL:`, siteConfig.siteUrl);

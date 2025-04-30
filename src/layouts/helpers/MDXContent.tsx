158fb06001dae3c715e4e3975edf2d5a6df6e802
marcpster <15250562+marcpster@users.noreply.github.com>
2025-04-30 18:07:24 +0100

    Lint more

--- src/layouts/helpers/MDXContent.tsx
+++ src/layouts/helpers/MDXContent.tsx
@@ -2,9 +2,12 @@ import shortcodes from "@/shortcodes/all";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MDXContent = ({ content }: { content: any }) => {
   interface IMdxOptions {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     remarkPlugins?: any[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     rehypePlugins?: any[];
   }
   const mdxOptions: IMdxOptions = {
@@ -13,7 +16,7 @@ const MDXContent = ({ content }: { content: any }) => {
 
   return (
     <>
-      {/* @ts-ignore */}
+      {/* @ts-ignore @MP fixme */}
       <MDXRemote
         source={content}
         components={shortcodes}
--- src/layouts/shortcodes/all.tsx
+++ src/layouts/shortcodes/all.tsx
@@ -1,17 +1,11 @@
 import Accordion from "./Accordion";
 import Button from "./Button";
 import Notice from "./Notice";
-import Tab from "./Tab";
-import Tabs from "./Tabs";
-import Video from "./Video";
 import Testimonial from "./Testimonial";
 
 const shortcodes = {
   Button,
   Accordion,
-  Video,
-  Tab,
-  Tabs,
   Notice,
   Testimonial
 };

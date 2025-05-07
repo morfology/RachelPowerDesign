fa907cba5a01015853deffc1ffab3554ec950a17
marcpster <15250562+marcpster@users.noreply.github.com>
2025-05-07 18:31:35 +0100

    Switch off lint on production again

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
   output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors. @MP Reenable later!
-    //ignoreDuringBuilds: true,
+    // your project has ESLint errors. @MP
+    ignoreDuringBuilds: true,
   },
 };
 
--- src/lib/taxonomyParser.ts
+++ src/lib/taxonomyParser.ts
@@ -1,25 +1,21 @@
 import { getSinglePage } from "@/lib/contentParser";
 import { slugify } from "@/lib/utils/textConverter";
 
+
 // get all taxonomies from frontmatter
 export const getTaxonomy = (folder: string, name: string): Array<string> => {
-  const singlePages = getSinglePage(folder);
-  const taxonomyPages = singlePages.map((page) => page.frontmatter.categories);
-  const taxonomies = [];
-  for (let i = 0; i < taxonomyPages?.length; i++) {
-    const taxonomyArray = taxonomyPages[i];
-    for (let j = 0; j < taxonomyArray?.length; j++) {
-      taxonomies.push(slugify(taxonomyArray[j]));
-    }
-  }
+
+  const taxonomies = getAllTaxonomy(folder, name);
+
+  // Dedupe
   const taxonomy = [...new Set(taxonomies)];
 
   return taxonomy;
 };
 
 // @MP 2024-01-02 tags[] to by dynamic based on name?
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const getAllTaxonomy = (folder: string, name: string) => {
+export const getAllTaxonomy = (folder: string, name: string): Array<string> => {
+
   const singlePages = getSinglePage(folder);
   const taxonomyPages = singlePages.map((page) => page.frontmatter.categories);
   const taxonomies = [];

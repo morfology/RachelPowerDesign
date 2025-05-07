c5d32c4b01238363d55a1fc7e03f62bea2864b76
marcpster <15250562+marcpster@users.noreply.github.com>
2025-05-07 13:47:10 +0100

    rem unused config

--- src/layouts/partials/Footer.tsx
+++ src/layouts/partials/Footer.tsx
@@ -1,15 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import Logo from "@/components/Logo";
 import Social from "@/components/Social";
-import config from "@/config/config.json";
+
 import menu from "@/config/menu.json";
 import social from "@/config/social.json";
-import { markdownify } from "@/lib/utils/textConverter";
-import Link from "next/link";
 
 const Footer = () => {
-  const { copyright } = config.params;
 
   return (
     <footer className="bg-theme-light ">

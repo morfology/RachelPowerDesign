58dc0dfd5108427a7f78bb9d5bca4ffb2f56483d
marcpster <15250562+marcpster@users.noreply.github.com>
2025-05-12 11:39:43 +0100

    Remove -

--- src/lib/utils/textConverter.ts
+++ src/lib/utils/textConverter.ts
@@ -32,6 +32,7 @@ export const humanize = (content: string) : string => {
   return content
     .replace(/^[\s_]+|[\s_]+$/g, "")
     .replace(/[_\s]+/g, " ")
+    .replace(/-+/g, " ")
     .replace(/^[a-z]/, function (m) {
       return m.toUpperCase();
     });

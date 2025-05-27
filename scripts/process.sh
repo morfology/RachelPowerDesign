9e1061ffc8ee402758480db5399513fa03de0732
marcpster <15250562+marcpster@users.noreply.github.com>
2025-05-27 17:37:58 +0100

    WIP script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Directory to search
-img_src=~/Downloads/Website_photos/Boundstone
-img_dest=~/git/RPD/public/images/farnham
+img_src=~/Downloads/Website_photos
+img_dest=~/git/RPD/public/images
 
 myfn() {
   if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
@@ -11,19 +11,13 @@ myfn() {
   fi
 
   width="$3"
-  src="$img_src/$1"
-  dest="$img_src/$2-$width.webp"
+  src="$1"
+  dest="$2-$width.webp"
 
   rm -f $dest
   magick  "$src" -resize "$width" "$dest"
   identify $dest
-  # echo "✅ Converted $src → $dest at width $width px"
 }
 
 
-# ls $img_src/0mt9ad8vmi3ppd006jay8w.jpg
-# ls .
-
-# cwebp -q 80 $img_src/0mt9ad8vmi3ppd006jay8w.jpg -o $img_src/output.webp
-
-myfn 0mt9ad8vmi3ppd006jay8w.jpg output "2880"
+myfn "$img_src/Boundstone/0mt9ad8vmi3ppd006jay8w.jpg" $img_dest/farnham/hero "2880"

e0b3dee110ff00e33941ba516a397158ca64ec22
marcpster <15250562+marcpster@users.noreply.github.com>
2025-05-27 17:33:21 +0100

    WIP image script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 # Directory to search
-search_dir="/Users/marcpower/git/RPD/public/images"
 img_src=~/Downloads/Website_photos/Boundstone
 img_dest=~/git/RPD/public/images/farnham
 
-# This image is 2880x1837
-identify $img_dest/living-1-2880.webp
+myfn() {
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: myfn <source_file_name> <dest_file_base> <width>"
+    return 1
+  fi
+
+  width="$3"
+  src="$img_src/$1"
+  dest="$img_src/$2-$width.webp"
+
+  rm -f $dest
+  magick  "$src" -resize "$width" "$dest"
+  identify $dest
+  # echo "✅ Converted $src → $dest at width $width px"
+}
+
 
 # ls $img_src/0mt9ad8vmi3ppd006jay8w.jpg
 # ls .
 
 # cwebp -q 80 $img_src/0mt9ad8vmi3ppd006jay8w.jpg -o $img_src/output.webp
 
-
-# Find all files with "detail" in their name
-# find "$search_dir" -type f -name "*detail*" | while read -r file; do
-#   echo "{ \"image\": \"$file\" },"
-# done
+myfn 0mt9ad8vmi3ppd006jay8w.jpg output "2880"

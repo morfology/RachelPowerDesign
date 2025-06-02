606ea1f22cb6291e634da1e69f6e2757ba6c26bc
marcpster <15250562+marcpster@users.noreply.github.com>
2025-06-02 21:51:56 +0100

    footer with logo at centre, stacking on small screen

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -13,8 +13,20 @@ const Footer = () => {
     <footer className="bg-theme-light ">
       <div className="container">
         <div className="row items-center py-10">
-          <div className="mb-8 text-center lg:col-3 lg:mb-0 lg:text-left">
-            <ul className="text-sm text-gray-500 flex flex-wrap justify-center gap-4">
+
+          {/* Logo: First on mobile (stacked), center on all screens */}
+          <div className="mb-8 order-1 text-center lg:order-2 lg:col-6 lg:mb-0">
+            <Logo />
+          </div>
+
+          {/* Socials: Second on mobile, right on lg */}
+          <div className="mb-8 order-2 text-center lg:order-3 lg:col-3 lg:mb-0 lg:mt-0 lg:text-right">
+            <Social source={social.main} className="social-icons" />
+          </div>
+
+          {/* Policies: Last on mobile, left on lg */}
+          <div className="mb-8 order-3 text-center lg:order-1 lg:col-3 lg:mb-0 lg:text-left">
+            <ul className="text-sm text-gray-500 flex flex-wrap justify-center gap-4 lg:justify-start">
               <li>
                 <Link href="/privacy-policy" className="hover:text-gray-700 transition-colors">
                   Privacy Policy
@@ -27,15 +39,11 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="mb-8 text-center lg:col-6 lg:mb-0">
-            <Logo />
-          </div>
-          <div className="mb-8 text-center lg:col-3 lg:mb-0 lg:mt-0 lg:text-right">
-            <Social source={social.main} className="social-icons" />
-          </div>
+
         </div>
       </div>
     </footer>
+
   );
 };
 

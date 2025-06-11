d02ae05986820221e0f2603770746eb3bf4bc567
marcpster <15250562+marcpster@users.noreply.github.com>
2025-06-11 15:29:39 +0100

    Contact testimonial and metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,126 +1,31 @@
-"use client";
-//// import { Metadata } from "next";
-//import ContactForm from "./ContactForm";
+import MDXContent from "@/helpers/MDXContent";
+import { getPageFromPath } from "@/lib/contentParser";
+import PageHeader from "@/components/PageHeader";
+import ContactForm from "@/components/ContactForm";
+import { getPostMetadata } from "@/lib/pageMeta";
 
-// export const metadata: Metadata = {
-//   title: "Contact Us",
-//   description: "Get in touch with us",
-//   openGraph: {
-//     title: "Contact Us",
-//     description: "Get in touch with us",
-//     type: "website",
-//   },
-// };
+const page = getPageFromPath(`contact/_index.md`);
 
+// Get metadata for the page
+export const generateMetadata = () => getPostMetadata(page);
+
+// ContactPage page component
 export default function ContactPage() {
-  return <ContactForm />;
+
+  return <>
+    <PageHeader heading={page.frontmatter.heading} />
+    <section className="contact">
+      <ContactForm />
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <MDXContent content={page.content} />
+        </div>
+      </div>
+    </section>
+  </>;
 }
 
 
-import PageHeader from "@/components/PageHeader";
-import { Formik, Form } from "formik";
-import { useState } from "react";
-
-const ContactForm = () => {
-  const contact_form_action = "https://form-handler-production.up.railway.app/submit-form"
-  type StatusType = "success" | "error" | null;
-  const [status, setStatus] = useState<StatusType>(null);
-
-  return (
-    <>
-      <PageHeader heading={"Contact Us"} />
-      <section className="contact">
-      <div className="container pt-10 pb-10">
-        <Formik
-          initialValues={{ name: "", email: "", message: "" }}
-          onSubmit={async (values, { resetForm }) => {
-            setStatus(null);
-
-            try {
-              const response = await fetch(contact_form_action, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                },
-                body: new URLSearchParams(values).toString(),
-              });
-
-
-              if (response.ok) {
-                setStatus("success");
-                resetForm();
-              } else {
-                setStatus("error");
-              }
-            } catch (err) {
-              console.error(err);
-              setStatus("error");
-            }
-          }}
-        >
-          {({ handleChange, values }) => (
-            <Form className="max-w-sm mx-auto">
-              <div className="mb-5">
-                <label className="form-label" htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  onChange={handleChange}
-                  value={values.name}
-                  className="form-input"
-                  placeholder="Your name"
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label className="form-label" htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  onChange={handleChange}
-                  value={values.email}
-                  className="form-input"
-                  placeholder="name@company.com"
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label className="form-label" htmlFor="message">Message</label>
-                <textarea
-                  id="message"
-                  name="message"
-                  onChange={handleChange}
-                  value={values.message}
-                  className="form-input"
-                  placeholder="Your message"
-                  required
-                />
-              </div>
-
-              <button type="submit" className="btn btn-primary">Submit</button>
-
-              <div className="h-16 w-full flex items-center justify-center overflow-hidden">
-
-                {status === "success" && (
-                  <p className="mt-4 font-bold text-xl ">Message sent successfully!</p>
-                )}
-
-                {status === "error" && (
-                  <p className="mt-4 text-red-500">Something went wrong. Please try again.</p>
-                )}
-              </div>
-
-            </Form>
-          )}
-        </Formik>
-        </div>
-      </section>
-    </>
-  );
-};
-
-//export default ContactForm; 
--- src/content/contact/_index.md
+++ src/content/contact/_index.md
@@ -1,6 +1,8 @@
 ---
-heading: "Contact us"
-title: ""
-description: "this is meta description"
-draft: false
+heading: "Contact Us"
+title: "Contact Us"
+description: "Get in touch with our interior design team for consultations, project enquiries, or to discuss your home transformation. We'd love to hear from you."
 ---
+<Testimonial link="https://www.houzz.co.uk/viewReview/1868678/rachel-power-design-review">
+Rachel did an amazing job for us on our house redesign and refurbishment. Her attention to detail, creativity / internal design and managing to a budget and deadlines was very impressive. Rachel did all this in a relaxed, thoughtful and attentive way so we ended with a design that we loved. She also challenged our thoughts in a great way. We would highly recommend her.
+</Testimonial>
